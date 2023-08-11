87786a330e62cae1a19cbb003032d5e01e690f30
Evgesha <Slav.novikova2014@gmail.com>
2023-08-11 10:25:25 +0700

    changed CollBackTest

--- src/test/java/ru/netology/CollbackTest.java
+++ src/test/java/ru/netology/CollbackTest.java
@@ -42,7 +42,6 @@ public class CollbackTest {
     @Test
     void shouldTestV2() {
         driver.get("http://localhost:9999");
-        // WebElement form = driver.findElement(By.cssSelector("[class='form form_size_m form_theme_alfa-on-white']"));
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Евгения");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
